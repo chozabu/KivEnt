32352545c44650db26c97dbd996a4a580fb4e9ea
Chozabu <chozabu@gmail.com>
2015-02-07 13:43:53 +0000

    update tut3 to work with new mem model

--- kivent_tutorials/3b_adding_poly_physics_objects/main.py
+++ kivent_tutorials/3b_adding_poly_physics_objects/main.py
@@ -6,9 +6,11 @@ from random import randint, choice
 from math import radians, pi, sin, cos
 import kivent_core
 import kivent_cymunk
-from kivent_core.renderers import texture_manager, VertMesh
+from kivent_core.resource_managers import texture_manager, model_manager
+from kivent_core.vertmesh import VertMesh
 
 texture_manager.load_atlas('assets/background_objects.atlas')
+
 
 class TestGame(Widget):
     def __init__(self, **kwargs):
@@ -40,6 +42,7 @@ class TestGame(Widget):
             self.gameworld.systems['renderer'].rebatch_entity(ent_id)
 
     def draw_some_stuff(self):
+        self.draw_regular_polygon(8, 30., (1., 0., 0., 1.), 'tmesh')
         size = Window.size
         self.created_entities = created_entities = []
         entities = self.gameworld.entities
@@ -47,24 +50,41 @@ class TestGame(Widget):
             pos = (randint(0, size[0]), randint(0, size[1]))
             ent_id = self.create_asteroid(pos)
             created_entities.append(ent_id)
+        vert_mesh = model_manager.vert_mesh_from_key('tmesh')
+        print "vert_mesh=",vert_mesh
+        print dir(vert_mesh)
+        print "vert_mesh.indices=",vert_mesh.indices
+        print "vert_mesh.vertex_count=",vert_mesh.vertex_count
+        print "vert_mesh.index_count=",vert_mesh.index_count
+        for i in range(vert_mesh.vertex_count):
+			print vert_mesh[i]
         Clock.schedule_interval(self.destroy_created_entity, 1.)
 
-    def draw_regular_polygon(self, sides, radius, color):
+    def draw_regular_polygon(self, sides, radius, color, key):
         x, y = 0., 0.
         angle = 2. * pi / sides
         all_verts = []
         all_verts_a = all_verts.append
         l_pos = list((x, y))
+        l_pos.extend(l_pos)#tex coords
+        l_pos.extend([0,0])#center
+        l_pos.extend([0])#
         l_pos.extend(color)
+        l_pos.extend([1])#scale
         all_verts_a(l_pos)
         triangles = []
         triangles_a = triangles.extend
         r = radius
         for s in range(sides):
-            new_pos = x + r * sin(s * angle), y + r * cos(s * angle)
+            print s * angle
+            new_pos = int(x + r * sin(s * angle)), int(y + r * cos(s * angle))
             l_pos = list(new_pos)
-            l_pos.extend((0., 0.))
+            l_pos.extend(new_pos)#tex coords
+            l_pos.extend([0,0])#center
+            l_pos.extend([0])#
             l_pos.extend(color)
+            l_pos.extend([1])#scale
+            print l_pos
             all_verts_a(l_pos)
             if s == sides-1:
                 triangles_a((s+1, 0, 1))
@@ -73,11 +93,19 @@ class TestGame(Widget):
         render_system = self.gameworld.systems['renderer']
         vert_count = len(all_verts)
         index_count = len(triangles)
-        vert_mesh =  VertMesh(render_system.attribute_count, 
-            vert_count, index_count)
+        #render_system.attribute_count = 12
+        print "render_system.attribute_count=", render_system.attribute_count
+        model_manager.load_mesh(render_system.attribute_count, vert_count, index_count, key)
+        vert_mesh = model_manager.vert_mesh_from_key(key)
+        #vert_mesh =  VertMesh(render_system.attribute_count, 
+        #    vert_count, index_count)
+        print "triangles"
         vert_mesh.indices = triangles
+        print triangles
+        print "verts"
         for i in range(vert_count):
             vert_mesh[i] = all_verts[i]
+            print all_verts[i]
         return vert_mesh
 
 
@@ -91,7 +119,6 @@ class TestGame(Widget):
         col_shape = {'shape_type': 'circle', 'elasticity': .5, 
             'collision_type': 1, 'shape_info': shape_dict, 'friction': 1.0}
         col_shapes = [col_shape]
-        vert_mesh = self.draw_regular_polygon(9, 30., (1., 0., 0., 1.))
         physics_component = {'main_shape': 'circle', 
             'velocity': (x_vel, y_vel), 
             'position': pos, 'angle': angle, 
@@ -100,8 +127,8 @@ class TestGame(Widget):
             'ang_vel_limit': radians(200), 
             'mass': 50, 'col_shapes': col_shapes}
         create_component_dict = {'physics': physics_component, 
-            'renderer': {#'texture': 'asteroid1', 
-            'vert_mesh': vert_mesh, 
+            'renderer': {'texture': 'asteroid1', 
+            'vert_mesh_key': 'tmesh', 
             #'size': (64, 64),
             'render': True}, 
             'position': pos, 'rotate': 0, 'color': (1., 0., 0., 1.),
@@ -117,6 +144,8 @@ class TestGame(Widget):
 
     def update(self, dt):
         self.gameworld.update(dt)
+        #if len(self.gameworld.entities)>10:
+		#	print dir(self.gameworld.entities[7])
 
     def setup_states(self):
         self.gameworld.add_state(state_name='main', 
